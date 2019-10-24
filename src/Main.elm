f28af9793f6fff06a59962aa1c85468a2470d007
Andrew Cline <andrew.cline77@gmail.com>
2019-10-24 08:59:35 -0500

    add navigation and some new styles

--- src/Main.elm
+++ src/Main.elm
@@ -10,7 +10,7 @@ import Html.Attributes exposing (..)
 
 
 main =
-    Browser.element
+    Browser.document
         { init = init
         , view = view
         , update = update
@@ -34,7 +34,7 @@ type alias Model =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { title = "Kents Woodworking Shop"
+    ( { title = "Kent's Woodworking Shop"
       , greeting = "Welcome, take a look around"
       }
     , Cmd.none
@@ -69,9 +69,30 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div [ class "bg-teal" ]
-        [ h1 [ class "" ] [ text model.title ]
-        , p [] [ text model.greeting ]
+    { title = model.title
+    , body =
+        [ navigation model
+        , landingPage model
+        ]
+    }
+
+
+navigation : Model -> Html Msg
+navigation model =
+    nav [ class "bg-red-600" ]
+        [ ul [ class "flex items-center justify-end h-10" ]
+            [ li [ class "mr-6 " ] [ a [ class "text-orange-100", href "#" ] [ text "Home" ] ]
+            , li [ class "mr-6" ] [ a [ class "text-orange-100", href "#" ] [ text "About" ] ]
+            , li [ class "mr-6" ] [ a [ class "text-orange-100", href "#" ] [ text "Projects" ] ]
+            ]
+        ]
+
+
+landingPage : Model -> Html Msg
+landingPage model =
+    div [ class ".container bg-orange-100 p-6" ]
+        [ h1 [ class "text-2xl mb-2" ] [ text model.title ]
+        , p [ class "text-gray-800" ] [ text model.greeting ]
         ]
